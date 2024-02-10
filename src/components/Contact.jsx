edc7be6458ec0d9e581cdd52a58a29a998feb9ec
Manisha Heera <nishahra@gmail.com>
2024-02-09 19:06:53 -0800

    Star background on contact form added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -48,10 +48,16 @@ const Contact = () => {
             <textarea rows="7" name="message" value={form.message} onChange={changeHandler} placeholder="Leave a message" className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium" />
           </label>
 
-          <button className="bg-tertiary py-3 px-8 shadow-md shadow-primary outline-none rounded-xl text-white w-fit" type="submit"> {loading? "Submitting..." : "Submit"} </button>
+          <button className="bg-tertiary py-3 px-8 shadow-md font-bold shadow-primary outline-none rounded-xl text-white w-fit" type="submit"> {loading? "Submitting..." : "Submit"} </button>
 
 
         </form>
+
+      </motion.div>
+
+      <motion.div variants={slideIn("right", "tween", 0.2, 1)} className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+
+        <EarthCanvas />
 
       </motion.div>
     </div>
